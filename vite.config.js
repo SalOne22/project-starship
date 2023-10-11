91c98bdb23f7b3bd3f869ee9c02f58692d160ffa
Vladyslav Vladinov <111443297+SalOne22@users.noreply.github.com>
2023-10-12 00:04:10 +0200

    Fixed module resolution

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,5 @@
+/* eslint-env node */
+import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
@@ -7,7 +9,7 @@ export default defineConfig({
   base: '/project-starship/',
   resolve: {
     alias: {
-      '@': './src',
+      '@': path.join(__dirname, 'src'),
     },
   },
 });
