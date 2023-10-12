5433a38bb9a20cb598afb27062c3882487e27c42
Vladyslav Vladinov <111443297+SalOne22@users.noreply.github.com>
2023-10-12 21:11:37 +0200

    Added base layout

--- src/modules/Layout/Layout.jsx
+++ src/modules/Layout/Layout.jsx
@@ -1,10 +1,39 @@
+import { AppShell } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 import { Outlet } from 'react-router-dom';
 
+import SideBar from '@/modules/SideBar';
+
+import Header from './components/Header';
+
 function Layout() {
+  const [opened] = useDisclosure(false);
+
   return (
-    <div>
-      <Outlet />
-    </div>
+    <AppShell
+      layout="alt"
+      withBorder={false}
+      header={{
+        height: { base: 120, md: 132, xl: 116 },
+      }}
+      navbar={{
+        width: { base: 225, md: 289 },
+        breakpoint: 'xl',
+        collapsed: { mobile: !opened },
+      }}
+    >
+      <AppShell.Header>
+        <Header />
+      </AppShell.Header>
+
+      <AppShell.Navbar>
+        <SideBar />
+      </AppShell.Navbar>
+
+      <AppShell.Main>
+        <Outlet />
+      </AppShell.Main>
+    </AppShell>
   );
 }
 
