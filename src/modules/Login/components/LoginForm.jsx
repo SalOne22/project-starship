01f8d59ec576fae0a925d471b4c27718c1f7cde7
Vladyslav Vladinov <111443297+SalOne22@users.noreply.github.com>
2023-10-12 14:55:58 +0200

    Added LoginPage

---
+++ src/modules/Login/components/LoginForm.jsx
@@ -0,0 +1,5 @@
+function LoginForm() {
+  return <div>LoginForm</div>;
+}
+
+export default LoginForm;
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,5 @@
+function LoginPage() {
+  return <div>Login</div>;
+}
+
+export default LoginPage;
