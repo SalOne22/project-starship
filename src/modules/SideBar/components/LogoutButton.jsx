40aefccd1c07177a69952c0509dc448597562c30
Vladyslav Vladinov <111443297+SalOne22@users.noreply.github.com>
2023-10-13 16:52:42 +0200

    Added LogoutButton

--- src/modules/SideBar/components/LogoutButton.jsx
+++ src/modules/SideBar/components/LogoutButton.jsx
@@ -1,5 +1,27 @@
-function LogoutButton() {
-  return <div>LogoutButton</div>;
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import { Button } from '@mantine/core';
+import { IconLogout } from '@tabler/icons-react';
+
+import css from '../styles/LogoutButton.module.css';
+
+function LogoutButton({ className }) {
+  return (
+    <Button
+      className={clsx(css.logout, className)}
+      radius="lg"
+      rightSection={<IconLogout width={18} height={18} />}
+      classNames={{
+        section: css.section,
+      }}
+    >
+      Log out
+    </Button>
+  );
 }
+
+LogoutButton.propTypes = {
+  className: PropTypes.string,
+};
 
 export default LogoutButton;
