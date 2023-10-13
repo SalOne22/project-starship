bb417f44f00309f01535111179d9913e131e0c9d
Vladyslav Vladinov <111443297+SalOne22@users.noreply.github.com>
2023-10-13 17:18:10 +0200

    Layout (#3)
    * Added base layout
    
    * Added Logo component
    
    * Added CloseButton
    
    * Added clsx
    
    * Added UserNav
    
    * Added LogoutButton
    
    * Added SideBar

--- src/modules/SideBar/components/UserNav.jsx
+++ src/modules/SideBar/components/UserNav.jsx
@@ -1,5 +1,40 @@
+import { Box, Flex, Stack, Title } from '@mantine/core';
+import {
+  IconUserCheck,
+  IconCalendarCheck,
+  IconChartBar,
+} from '@tabler/icons-react';
+
+import NavigationLink from './NavigationLink';
+
+import css from '../styles/UserNav.module.css';
+
 function UserNav() {
-  return <div>UserNav</div>;
+  return (
+    <Box>
+      <Title order={3} className={css.title}>
+        User Panel
+      </Title>
+
+      <Flex direction="column" gap={{ base: 18, md: 16 }}>
+        <NavigationLink
+          to="/account"
+          title="My account"
+          icon={<IconUserCheck stroke={1.8} width={20} height={20} />}
+        />
+        <NavigationLink
+          to="/calendar"
+          title="Calendar"
+          icon={<IconCalendarCheck stroke={1.8} width={20} height={20} />}
+        />
+        <NavigationLink
+          to="/statistics"
+          title="Statistics"
+          icon={<IconChartBar stroke={1.8} width={20} height={20} />}
+        />
+      </Flex>
+    </Box>
+  );
 }
 
 export default UserNav;
