4978893a7c4eb8cb75a87c5fb7dce6d75d2c321e
Vladyslav Vladinov <111443297+SalOne22@users.noreply.github.com>
2023-10-14 14:54:24 +0200

    Added token refresh

--- src/modules/Layout/Layout.jsx
+++ src/modules/Layout/Layout.jsx
@@ -1,13 +1,30 @@
+import { useEffect } from 'react';
 import { AppShell } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
+import { useDispatch, useSelector } from 'react-redux';
 import { Outlet } from 'react-router-dom';
 
 import SideBar from '@/modules/SideBar';
 
+import { selectIsAuthenticated, selectToken } from '@/redux/slices/authSlice';
+import { refreshUserThunk } from '@/redux/operations';
+
 import Header from './components/Header';
 
 function Layout() {
+  const dispatch = useDispatch();
+  const token = useSelector(selectToken);
+  const isAuthenticated = useSelector(selectIsAuthenticated);
+
+  useEffect(() => {
+    if (!token || isAuthenticated) return;
+
+    dispatch(refreshUserThunk());
+  }, [dispatch, isAuthenticated, token]);
+
   const [opened, { close }] = useDisclosure();
+
+  if (!isAuthenticated) return <Outlet />;
 
   return (
     <AppShell
