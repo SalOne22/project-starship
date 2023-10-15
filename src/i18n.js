3368e8a874d404404e0e19d37d23c74c011a60d4
Vladyslav Vladinov <111443297+SalOne22@users.noreply.github.com>
2023-10-15 14:11:03 -0700

    fixed internationalization

--- src/i18n.js
+++ src/i18n.js
@@ -12,7 +12,7 @@ i18n
     fallbackLng: 'en',
     supportedLngs: ['en', 'ua'],
     backend: {
-      loadPath: '/locales/{{lng}}/translation.json',
+      loadPath: 'public/locales/{{lng}}/translation.json',
     },
   });
 
